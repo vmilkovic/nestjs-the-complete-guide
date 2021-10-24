34dea843ec0a1e503ce709281fc681d4070994e4
Vedran Milković <vedran.milkovic25@gmail.com>
2021-10-24 14:17:43 +0200

    Generating Projects with the Nest CLI

---
+++ messages/src/messages/messages.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return 'Messages';
+  }
+
+  @Post()
+  createMessage() {}
+
+  @Get('/:id')
+  getMessage() {}
+}
