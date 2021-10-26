c7a461f14dccbaa527cfe740cd8ff8973c9d7af0
Vedran Milković <vedran.milkovic25@gmail.com>
2021-10-26 20:42:52 +0200

    Nest Architecture: Services and Repositories

--- messages/src/messages/messages.module.ts
+++ messages/src/messages/messages.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
-
+import { MessagesService } from './messages.service';
+import { MessagesRepository } from './messages.repository';
 @Module({
   controllers: [MessagesController],
+  providers: [MessagesService, MessagesRepository],
 })
 export class MessagesModule {}
---
+++ messages/src/messages/messages.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { MessagesRepository } from './messages.repository';
+
+@Injectable()
+export class MessagesService {
+  constructor(public messagesRepository: MessagesRepository) {}
+
+  findOne(id: string) {
+    return this.messagesRepository.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepository.findAll();
+  }
+
+  create(content: string) {
+    return this.messagesRepository.create(content);
+  }
+}
