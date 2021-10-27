3714e3f0562aaf5e36bb5f80419cb55b86309d33
Vedran Milković <vedran.milkovic25@gmail.com>
2021-10-27 19:49:37 +0200

    Persisting Data with TypeORM

---
+++ mycarvalue/src/users/users.controller.ts
@@ -0,0 +1,10 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    console.log(body);
+  }
+}
