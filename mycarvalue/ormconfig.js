5a26cfd3e5ff8b2e88a2d30f8c30abbcb8b9ee46
Vedran Milković <vedran.milkovic25@gmail.com>
2021-11-01 22:23:39 +0100

    Production Deployment

---
+++ mycarvalue/ormconfig.js
@@ -0,0 +1,40 @@
+var dbConfig = {
+  synchronize: false,
+  migrations: ['migrations/*.js'],
+  cli: {
+    migrationsDir: 'migrations',
+  },
+};
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.ts'],
+      migrationsRun: true,
+    });
+    break;
+  case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgress',
+      database: process.env.DATABASE_URL,
+      entities: ['**/*.entity.js'],
+      migrationsRun: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+    break;
+  default:
+    throw new Error('unknown environment');
+}
+
+module.exports = dbConfig;
--- mycarvalue/src/users/user.entity.ts
+++ mycarvalue/src/users/user.entity.ts
@@ -7,7 +7,7 @@ import {
   PrimaryGeneratedColumn,
   OneToMany,
 } from 'typeorm';
-import { Report } from 'src/reports/report.entity';
+import { Report } from '../reports/report.entity';
 
 @Entity()
 export class User {
